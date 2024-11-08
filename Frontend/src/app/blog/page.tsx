3b8c9659bdf287a98f6e392147ad97eccb9cacb3
NidhiPatel09 <nid.000059@gmail.com>
2024-11-08 18:02:05 -0500

    blog page update2

--- Frontend/src/app/blog/page.tsx
+++ Frontend/src/app/blog/page.tsx
@@ -111,29 +111,43 @@ export default function Blog() {
               <section className="flex justify-center items-center my-8">
                 <div className="bg-dark p-6 rounded shadow-md max-w-md w-full">
                   <h2 className="text-2xl font-bold mb-4 text-center">
-                    Upload Your Recipe
+                    Upload Your Blog
                   </h2>
                   <form className="flex flex-col gap-4">
-                    <input
-                      type="text"
-                      placeholder="Recipe Title"
-                      className="p-2 border rounded"
-                    />
-                    <input
-                      type="text"
-                      placeholder="Add Ingredients"
-                      className="p-2 border rounded"
-                    />
-                    <textarea
-                      placeholder="Add Steps"
-                      className="p-2 border rounded resize-none"
-                    ></textarea>
+                    {/* Icon in input field */}
+                    <div className="relative">
+                      <input
+                        type="text"
+                        placeholder="Title"
+                        className="p-2 pl-10 border rounded w-full" // Added padding-left for space for the icon
+                      />
+                      <i className="fas fa-pencil-alt absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-600"></i> {/* Positioned icon */}
+                    </div>
+
+                    <div className="relative">
+                      <input
+                        type="text"
+                        placeholder="Add Description"
+                        className="p-2 pl-10 border rounded w-full" // Padding-left for icon space
+                      />
+                      <i className="fas fa-pen absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-600"></i> {/* Positioned icon */}
+                    </div>
+
+                    <div className="relative">
+                      <textarea
+                        placeholder="Add More Details"
+                        className="p-2 pl-10 border rounded resize-none w-full" // Padding-left for icon space
+                      ></textarea>
+                      <i className="fas fa-align-left absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-600"></i> {/* Positioned icon */}
+                    </div>
+
                     <button className="group relative w-full flex justify-center mt-2 py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-green-500 hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">
-                      Submit Recipe
+                      Submit
                     </button>
                   </form>
                 </div>
               </section>
+
 
               </div>
               ) : (
